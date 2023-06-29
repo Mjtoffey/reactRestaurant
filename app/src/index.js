858274fe885e1380c28780d925926f6f8fea062a
Mjtoffey <mjtoffey@gmail.com>
2023-06-29 17:31:10 -0400

    updated files

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+import App from './App';
+import Ingredients from './routes/Ingredients';
+import Wizards from './routes/Wizards';
+import Elixirs from './routes/Elixirs';
+import Houses from './routes/Houses'
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // root element is in the index.html
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="elixirs" element={<Elixirs />} />
+          <Route path="houses" element={<Houses />} />
+          <Route path="wizards" element={<Wizards />} />
+          <Route path="ingredients" element={<Ingredients />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
